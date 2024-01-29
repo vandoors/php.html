1c062d35072fad394eb53faed9310383a73c69f6
vandoors <143453791+vandoors@users.noreply.github.com>
2024-01-28 19:44:46 -0600

    self-referencing postfullname.php

---
+++ labs/week2/postfullname.php
@@ -0,0 +1,25 @@
+<html>
+    <head>
+      <title>Full name form</title>
+    </head>
+    <body>
+        <?php if (!isset($_POST['submit'])) { ?>
+            <h2>Enter your full name</h2>
+            <form
+                action="<?= $_SERVER['PHP_SELF'] ?>"
+                method="POST"
+            >
+                First name:<br>
+                <input type="text" name="first_name"><br>
+                Last name:<br>
+                <input type="text" name="last_name"><br>
+                <p>
+                <input type="submit" name="submit" value="Submit Name">
+            </form>
+        <?php } else { ?>
+            <h2>:)</h2>
+            Hello <?= $POST['first_name'] . " " . $_POST['last_name'] ?>
+            Thank you for submitting your name.
+        <?php } ?>
+    </body>
+</html>
