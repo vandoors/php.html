ef97d44d094647d847e6b1026d920b9cefb2c81c
vandoors <143453791+vandoors@users.noreply.github.com>
2024-01-28 20:00:52 -0600

    badlib pt 1 first attempt

---
+++ labs/week2/badlibs.php
@@ -0,0 +1,26 @@
+<html>
+    <head>
+        <title>Badlibs (part 1)</title>
+    </head>
+    <body>
+        <?php if (!isset($_POST['submit'])) { ?>
+            <form action="<?= $_SERVER['PHP_SELF'] ?>" method="POST">
+                <label for="name">Noun</label>
+                <input type="text" id="name" name="noun"><br>
+
+                <label for="verb">Verb</label>
+                <input type="text" id="verb" name="verb"><br>
+
+                <label for="adverb">Adverb</label>
+                <input type="text" id="adverb" name="adverb"><br>
+
+                <label for="adjective">Adjective</label>
+                <input type="text" id="adjective" name="adjective"><br>
+
+                <input type="submit" name="submit" value="Generate Badlib">
+            </form>
+        <?php } else { ?>
+            <?= "The grumpy $_POST['adjective'] goblin $_POST['verb'] the sparkly $_POST['noun'] very $_POST['adverb']." ?>
+        <?php } ?>
+    </body>
+</html>
