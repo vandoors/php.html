a07bbf0b218f9478e08d2befe933ba19aa389b7b
vandoors <143453791+vandoors@users.noreply.github.com>
2024-02-05 11:58:57 -0600

    arrays.php pt 1

---
+++ labs/week3/arrays.php
@@ -0,0 +1,12 @@
+<?php
+
+$book_titles = [
+    "The Hunger Games",
+    "Catching Fire",
+    "Mockingjay",
+    "The Ballad of Songbirds and Snakes",
+    "Gone Girl"
+];
+
+echo $book_titles[2];
+echo $book_titles[3];
