2ba40c94c8dd28cf56e71397072c8ef93601b826
vandoors <143453791+vandoors@users.noreply.github.com>
2024-02-05 12:08:52 -0600

    arrays pt 2

--- labs/week3/arrays.php
+++ labs/week3/arrays.php
@@ -9,3 +9,13 @@ $book_titles = [
 ];
 
 echo $book_titles[2] . ", " . $book_titles[3];
+
+$books = [
+    "The Ballad of Songbirds and Snakes" => "Suzanne Collins",
+    "Gone Girl" => "Gillian Flynn",
+    "This Is How You Lose the Time War" => "Amal El-Mohtar & Max Gladstone"
+];
+
+for ($i = 0; $i < count($book_titles); $i++) {
+    echo $book_titles[$i] . " by " . $books[$book_titles[$i]], PHP_EOL; // PHP_EOL -> https://stackoverflow.com/a/20749472
+}
