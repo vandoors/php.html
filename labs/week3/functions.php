21b5fce6f27b104166d1f0bf38f7923212f323a6
vandoors <143453791+vandoors@users.noreply.github.com>
2024-02-05 13:00:46 -0600

    functions pt 1

---
+++ labs/week3/functions.php
@@ -0,0 +1,26 @@
+<?php
+
+function addTwoNumbers($num1, $num2)
+{
+    return $num1 + $num2;
+}
+
+function subtractTwoNumbers($num1, $num2)
+{
+    return $num1 - $num2;
+}
+
+function multiplyTwoNumbers($num1, $num2)
+{
+    return $num1 * $num2;
+}
+
+function divideTwoNumbers($num1, $num2)
+{
+    return $num1 / $num2;
+}
+
+echo addTwoNumbers(1, 3) . "<br>";
+echo subtractTwoNumbers(1, 3) . "<br>";
+echo multiplyTwoNumbers(1, 3) . "<br>";
+echo divideTwoNumbers(1, 3) . "<br>";
