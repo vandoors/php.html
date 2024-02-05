f17d1e788ed57ff83abd1e203c8bc4ef0ac8a710
vandoors <143453791+vandoors@users.noreply.github.com>
2024-02-05 12:41:53 -0600

    duh

--- labs/week3/loops.php
+++ labs/week3/loops.php
@@ -5,7 +5,7 @@
     <body>
         <table>
             <?php
-                for ($i = 1; $i = 10; $i++) {
+                for ($i = 1; $i <= 10; $i++) {
                     echo "<tr><td>" . $i . "</td><td>" . $i * $i . "</td></tr>";
                 }
             ?>
