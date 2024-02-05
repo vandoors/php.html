5a7a1c77bc82fdeae4eedcd659a37342e00384ef
vandoors <143453791+vandoors@users.noreply.github.com>
2024-02-05 13:09:33 -0600

    pt 2

--- labs/week3/functions.php
+++ labs/week3/functions.php
@@ -24,3 +24,14 @@ echo addTwoNumbers(1, 3) . "<br>";
 echo subtractTwoNumbers(1, 3) . "<br>";
 echo multiplyTwoNumbers(1, 3) . "<br>";
 echo divideTwoNumbers(1, 3) . "<br>";
+
+echo "<br>";
+
+function determineSalesTax($price, $tax_rate)
+{
+    return $price * $tax_rate;
+}
+
+$costOfItem = 100;
+$pretendTaxRate = 0.07;
+echo "Sales tax on your $" . $costOfItem . " item, taxed at " . $pretendTaxRate * 100 . "%, is $" . determineSalesTax($costOfItem, $pretendTaxRate);
