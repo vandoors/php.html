37539d1961592b1398f1860c411dd9a1650fabdd
vandoors <143453791+vandoors@users.noreply.github.com>
2024-02-15 16:23:00 -0600

    comment out server component (test)

--- projects/project1/badlibs.php
+++ projects/project1/badlibs.php
@@ -25,11 +25,11 @@
         $adjective = $_POST['adjective'];
         $adverb = $_POST['adverb'];
 
-        $dbc = mysqli_connect('localhost', 'student', 'student', 'Badlibs')
+/*        $dbc = mysqli_connect('localhost', 'student', 'student', 'Badlibs')
                 or trigger_error('Error connecting to MySQL server.', E_USER_ERROR);
 
         $query = "INSERT INTO badlibs (noun, verb, adjective, adverb)"
-            . "VALUES ('$noun', '$verb', '$adjective', '$adverb)";
+                . "VALUES ('$noun', '$verb', '$adjective', '$adverb)";
 
         $result = mysqli_query($dbc, $query)
                 or trigger_error('Error querying database.', E_USER_WARNING);
@@ -40,7 +40,7 @@
                 . mysqli_error($dbc), E_USER_WARNING);
         }
 
-        mysqli_close($dbc);
+        mysqli_close($dbc);*/
 
         echo "The $adjective $noun $verb $adverb. That's hilarious!";
     ?>
