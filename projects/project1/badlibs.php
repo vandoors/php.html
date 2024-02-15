52c586c16b6d95f95201a2feceab9717087e3034
vandoors <143453791+vandoors@users.noreply.github.com>
2024-02-15 17:32:43 -0600

    style

--- projects/project1/badlibs.php
+++ projects/project1/badlibs.php
@@ -9,19 +9,53 @@
 </head>
 <body>
     <form action="<?= $_SERVER['PHP_SELF'] ?>" method="POST">
-        <label for="name">Noun</label>
-        <input type="text" id="name" name="noun"><br>
+        <div className="form-group">
+            <div className="form-group-header">
+                <label for="name">Noun</label>
+            </div>
 
-        <label for="verb">Verb (past tense)</label>
-        <input type="text" id="verb" name="verb"><br>
+            <div className="form-group-body">
+                <input className="form-control" type="text" id="name" name="noun">
+            </div>
+        </div>
 
-        <label for="adjective">Adjective</label>
-        <input type="text" id="adjective" name="adjective"><br>
+        <div className="form-group">
+            <div className="form-group-header">
+                <label for="verb">Verb (past tense)</label>
+            </div>
 
-        <label for="adverb">Adverb</label>
-        <input type="text" id="adverb" name="adverb"><br>
+            <div className="form-group-body">
+                <input className="form-control" type="text" id="verb" name="verb">
+            </div>
+        </div>
 
-        <input type="submit" name="submit" value="Generate Badlib">
+        <div className="form-group">
+            <div className="form-group-header">
+                <label for="adjective">Adjective</label>
+            </div>
+
+            <div className="form-group-body">
+                <input className="form-control" type="text" id="adjective" name="adjective">
+            </div>
+        </div>
+
+        <div className="form-group">
+            <div className="form-group-header">
+                <label for="adverb">Adverb</label>
+            </div>
+
+            <div className="form-group-body">
+                <input className="form-control" type="text" id="adverb" name="adverb">
+            </div>
+        </div>
+
+        <div className="form-actions">
+            <div className="form-group">
+                <button className="btn btn-primary" type="submit" name="submit">
+                    Generate story
+                </button>
+            </div>
+        </div>
     </form>
 
     <hr>
