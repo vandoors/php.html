718944aa01f28f229536f61cdb644e518a7b1e55
vandoors <143453791+vandoors@users.noreply.github.com>
2024-02-15 17:17:36 -0600

    desc

--- projects/project1/badlibs.php
+++ projects/project1/badlibs.php
@@ -25,7 +25,7 @@
         $dbc = mysqli_connect('localhost', 'student', 'student', 'Badlibs')
                 or trigger_error('Error connecting to MySQL server.', E_USER_ERROR);
 
-        $query = "SELECT `id`, `constructed_story` FROM `badlibs` ORDER BY `id`";
+        $query = "SELECT `id`, `constructed_story` FROM `badlibs` ORDER BY `id` DESC";
 
         $result = mysqli_query($dbc, $query)
                 or trigger_error('Error querying database.', E_USER_WARNING);
