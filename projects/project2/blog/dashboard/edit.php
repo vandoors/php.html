2094cceb26a681c2925e2d405000e66d952554ac
vandoors <143453791+vandoors@users.noreply.github.com>
2024-03-19 18:49:08 -0500

    editing posts

---
+++ projects/project2/blog/dashboard/edit.php
@@ -0,0 +1,96 @@
+<?php
+    require_once('auth.php');
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link href="https://unpkg.com/@primer/css@^20.2.4/dist/primer.css" rel="stylesheet">
+
+    <title>Edit post</title>
+</head>
+<body class="m-6">
+    <?php
+        require_once('../dbconnection.php');
+        $dbc = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME)
+                or trigger_error('Error connecting to MySQL server for DB_NAME.',
+                        E_USER_ERROR);
+        
+        if (isset($_GET['id']))
+        {
+            $id = $_GET['id'];
+            $query = "SELECT title, date, content FROM posts WHERE id = $id";
+            $result = mysqli_query($dbc, $query)
+                    or trigger_error('Error querying database: failed to fetch post.',
+                    E_USER_ERROR);
+            
+            if (mysqli_num_rows($result) == 1)
+            {
+                $row = mysqli_fetch_assoc($result);
+
+                $post_title = $row['title'];
+                $post_date = $row['date'];
+                $post_content = $row['content'];
+            }
+        }
+        elseif (isset($_POST['[update_post]'], $_POST['post_title'],
+                        $_POST['post_date'], $_POST['post_content']))
+        {
+            $id = $_POST['id'];
+            $post_title = $_POST['post_title'];
+            $post_date = $_POST['post_date'];
+            $post_content = $_POST['post_content'];
+
+            $query = "UPDATE posts SET title = '$post_title', "
+                    . " date = '$post_date', content = '$post_content' "
+                    . "WHERE id = $id";
+
+            mysqli_query($dbc, $query)
+                or trigger_error(
+                    'Error querying database: failed to update post.',
+                    E_USER_ERROR);
+        
+            $nav_link = '../post.php?id=' . $id;
+            header("Location: $nav_link");
+        }
+        else
+        {
+            header("Location: index.php");
+        }
+    ?>
+    <form method="POST" action="<?= $_SERVER['PHP_SELF'] ?>">
+        <div class="form-group">
+            <div class="form-group-header">
+                <label for="postTitle" class="form-label">Title</label>
+            </div>
+            <div class="form-group-body">
+                <input type="text" maxlength="255" class="form-control" id="postTitle" name="post_title" placeholder="New post" value="<?= isset($_POST['post_title']) ? htmlspecialchars($_POST['post_title']) : '' ?>">
+            <div>
+        </div>
+
+        <div class="form-group">
+            <div class="form-group-header">
+                <label for="postDate" class="form-label">Date</label>
+            </div>
+            <div class="form-group-body">
+                <input type="date" class="form-control" id="postDate" name="post_date" value="<?= isset($_POST['post_date']) ? htmlspecialchars($_POST['post_date']) : '' ?>">
+            <div>
+        </div>
+
+        <div class="form-group">
+            <div class="form-group-header">
+                <label for="postContent" class="form-label">Content</label>
+            </div>
+            <div class="form-group-body">
+                <textarea class="form-control" id="postContent" name="post_content" rows="3"><?= isset($_POST['post_content']) ? htmlspecialchars($_POST['post_content']) : '' ?></textarea>
+            <div>
+        </div>
+
+        <button type="submit" class="btn mt-3" name="update_post">Update post</button>
+
+        <input type="hidden" name="id" value="<?= $id ?>">
+    </form>
+</body>
+</html>
