0d7523e1969329b4d39ad2e47b99dd4b8c2445de
vandoors <143453791+vandoors@users.noreply.github.com>
2024-03-19 15:24:36 -0500

    get more started on blog

--- projects/project2/blog/index.php
+++ projects/project2/blog/index.php
@@ -1,1 +1,11 @@
-<p>hi!</p>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Blog (project 2)</title>
+</head>
+<body>
+    <h1>Blog</h1>
+</body>
+</html>
