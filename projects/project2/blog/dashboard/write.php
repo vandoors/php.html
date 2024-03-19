6ea189f4668af6154bf9d8a1d0bd40c46bc4aeea
vandoors <143453791+vandoors@users.noreply.github.com>
2024-03-19 16:36:38 -0500

    fix

--- projects/project2/blog/dashboard/write.php
+++ projects/project2/blog/dashboard/write.php
@@ -11,7 +11,7 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 
     <?php
-        if if (!$display_new_post_form && isset($post_title))
+        if (!$display_new_post_form && isset($post_title))
         {
     ?>
     <title>New: "<?= $post_title ?>"</title>
