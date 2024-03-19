cf282958afbc45fd8e5784c0efc9bd61e45a9e02
vandoors <143453791+vandoors@users.noreply.github.com>
2024-03-19 15:33:23 -0500

    update auth response

--- projects/project2/blog/dashboard/auth.php
+++ projects/project2/blog/dashboard/auth.php
@@ -2,17 +2,13 @@
     $username = 'student';
     $password = 'student';
 
-    // IF Password OR Username are empty
-    //  OR Password  OR Username don't match
-    // send HTTP authentication headers
     if (!isset($_SERVER['PHP_AUTH_USER']) || !isset($_SERVER['PHP_AUTH_PW'])
         || $_SERVER['PHP_AUTH_USER'] !== $username
         || $_SERVER['PHP_AUTH_PW'] !== $password)
     {
-
         header('HTTP/1.1 401 Unauthorized');
         header('WWW-Authenticate: Basic realm="Student Listing"');
-        $invalid_response = "<h2>Students</h2><h4>You must enter a "
-                            . "valid username and password to access this page.</h4>";
+        $invalid_response = "<h1>Authentication Error</h1>"
+                            . "<p>Invalid username or password. Please try again.</p>";
         exit($invalid_response);
     }
