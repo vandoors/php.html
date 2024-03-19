b05e3c7a7ecd5e75443246617851ab7cac606d92
vandoors <143453791+vandoors@users.noreply.github.com>
2024-03-19 17:53:51 -0500

    dashboard list posts

--- projects/project2/blog/dashboard/index.php
+++ projects/project2/blog/dashboard/index.php
@@ -15,5 +15,43 @@
     <h1 class="h2">Dashboard</h1>
     <p>Welcome to the blog dashboard. Here you can create, edit, and delete posts.</p>
     <a href="write.php" class="btn btn-primary btn-sm my-2">New post</a>
+
+    <h2 class="h3 mt-6">Posts</h2>
+    <?php
+        require_once('../dbconnection.php');
+
+        $dbc = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME)
+                    or trigger_error('Error connecting to MySQL server for'
+                    .  DB_NAME, E_USER_ERROR);
+        
+        $query = "SELECT id, title, date "
+                . "FROM posts ORDER BY date DESC";
+        
+        $result = mysqli_query($dbc, $query)
+                or trigger_error('Error querying database: failed to fetch posts.', 
+                E_USER_ERROR);
+        
+        if (mysqli_num_rows($result) > 0):
+    ?>
+    <table>
+        <tbody>
+            <?php
+                while($row = mysqli_fetch_assoc($result))
+                {
+                    echo '<tr>';
+                    echo '<td><a href="post.php?id=' . $row['id'] . '">' . $row['title'] . '</a></td>';
+                    echo '<td>' . $row['date'] . '</td>';
+                    echo '</tr>';
+                }
+            ?>
+        </tbody>
+    </table>
+    <?php
+        else:
+    ?>
+    <p>No posts found.</p>
+    <?php
+        endif;
+    ?>
 </body>
 </html>
