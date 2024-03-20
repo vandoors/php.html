bdbbc2c1da528e5974fe49f9f475da591c31393d
vandoors <143453791+vandoors@users.noreply.github.com>
2024-03-19 19:53:06 -0500

    fix

--- projects/project2/blog/index.php
+++ projects/project2/blog/index.php
@@ -21,7 +21,7 @@
     <hr>
 
     <?php
-        require_once('../dbconnection.php');
+        require_once('dbconnection.php');
 
         $dbc = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME)
                     or trigger_error('Error connecting to MySQL server for'
