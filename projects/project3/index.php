d3cefbbb00508e8dd7297591108a0574e0312271
vandoors <143453791+vandoors@users.noreply.github.com>
2024-04-10 13:15:43 -0500

    header

---
+++ projects/project3/index.php
@@ -0,0 +1,11 @@
+<?php
+    require_once('./requires/page-titles.php');
+    $page_title = EXC_PROFILE;
+?>
+<!doctype html>
+<html>
+    <?php require_once('./requires/head.php'); ?>
+    <body>
+        hi
+    </body>
+</html>
---
+++ projects/project3/requires/header.php
@@ -0,0 +1,9 @@
+<header>
+    <h1><?= $page_title ?></h1>
+    <nav>
+        <ul>
+            <li><a href=<?= dirname($_SERVER['PHP_SELF']) ?>>Home</a></li>
+            <li><a href="/profile/">Profile</a></li>
+        </ul>
+    </nav>
+</header>
