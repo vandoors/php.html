c3943d46261567d8640d38211156fbee4dbdec70
vandoors <143453791+vandoors@users.noreply.github.com>
2024-04-10 13:08:27 -0500

    start proj 3

---
+++ projects/project3/requires/head.php
@@ -0,0 +1,8 @@
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+    <title><? $page_title ?></title>
+
+    <link href="https://unpkg.com/@primer/css@^20.2.4/dist/primer.css" rel="stylesheet">
+</head>
