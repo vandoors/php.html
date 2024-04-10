03ba48d07651744e50478e12b59fffa7a753bfbb
vandoors <143453791+vandoors@users.noreply.github.com>
2024-04-10 14:45:25 -0500

    test fix

--- projects/project3/requires/signup/isset.php
+++ projects/project3/requires/signup/isset.php
@@ -17,9 +17,9 @@ if (
     && !empty($weight)
 )
 {
-    require_once('../dbconnection.php');
+    require_once('../../dbconnection.php');
 
-    require_once('../queryutils.php');
+    require_once('../../queryutils.php');
 
     $dbc = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME)
             or trigger_error(
