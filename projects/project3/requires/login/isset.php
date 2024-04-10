611f39ed8ce248816ee4f72f4d6298a1feeda71a
vandoors <143453791+vandoors@users.noreply.github.com>
2024-04-10 15:16:08 -0500

    fix

--- projects/project3/requires/login/isset.php
+++ projects/project3/requires/login/isset.php
@@ -1,57 +1,54 @@
 <?php
-if (empty($_SESSION['user_id']) && isset($_POST['login_submission']))
+// Get user name and password
+$user_name = $_POST['user_name'];
+$password = $_POST['password'];
+
+if (!empty($user_name) && !empty($password))
 {
-    // Get user name and password
-    $user_name = $_POST['user_name'];
-    $password = $_POST['password'];
+    require_once __DIR__ . '/../dbconnection.php';
+    require_once __DIR__ . '/../queryutils.php';
 
-    if (!empty($user_name) && !empty($password))
+    $dbc = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME)
+            or trigger_error(
+            'Error connecting to MySQL server for' . DB_NAME,
+            E_USER_ERROR );
+
+    // Check if user already exists
+    $query = "SELECT id, username, sh_password"
+            . "FROM exercise_user WHERE username = ?";
+
+    $results = parameterizedQuery($dbc, $query, 's', $username)
+            or trigger_error(mysqli_error($dbc), E_USER_ERROR);
+
+    // IF user was found, validate password
+    if (mysqli_num_rows($results) == 1)
     {
-        require_once __DIR__ . '/../dbconnection.php';
-        require_once __DIR__ . '/../queryutils.php';
+        $row = mysqli_fetch_array($results);
 
-        $dbc = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME)
-                or trigger_error(
-                'Error connecting to MySQL server for' . DB_NAME,
-                E_USER_ERROR );
-
-        // Check if user already exists
-        $query = "SELECT id, username, sh_password"
-                . "FROM exercise_user WHERE username = ?";
-
-        $results = parameterizedQuery($dbc, $query, 's', $username)
-                or trigger_error(mysqli_error($dbc), E_USER_ERROR);
-
-        // IF user was found, validate password
-        if (mysqli_num_rows($results) == 1)
+        if (password_verify($password, $row['password_hash']))
         {
-            $row = mysqli_fetch_array($results);
+            $_SESSION['id'] = $row['id'];
+            $_SESSION['username'] = $row['username'];
 
-            if (password_verify($password, $row['password_hash']))
-            {
-                $_SESSION['id'] = $row['id'];
-                $_SESSION['username'] = $row['username'];
-
-                // Redirect to the home page
-                $home_url = dirname($_SERVER['PHP_SELF']);
-                header('Location: ' . $home_url);
-            }
-            else
-            {
-                echo "<p class='color-fg-severe'>An incorrect username or password was entered.</p>";
-            }
-        }
-        else if (mysqli_num_rows($results) == 0) // User does not exist
-        {
-            echo "<p class='color-fg-severe'>An incorrect username or password was entered.</p>";
+            // Redirect to the home page
+            $home_url = dirname($_SERVER['PHP_SELF']);
+            header('Location: ' . $home_url);
         }
         else
         {
-            echo "<p class='color-fg-danger'>Something went wrong.</p>";
+            echo "<p class='color-fg-severe'>An incorrect username or password was entered.</p>";
         }
+    }
+    else if (mysqli_num_rows($results) == 0) // User does not exist
+    {
+        echo "<p class='color-fg-severe'>An incorrect username or password was entered.</p>";
     }
     else
     {
-        echo "<p class='color-fg-severe'>You must enter a value for all fields.</p>";
+        echo "<p class='color-fg-danger'>Something went wrong.</p>";
     }
+}
+else
+{
+    echo "<p class='color-fg-severe'>You must enter a value for all fields.</p>";
 }
