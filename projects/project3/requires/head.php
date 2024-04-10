9307730d8b02ffb4ed3738211df869e264d92ad5
vandoors <143453791+vandoors@users.noreply.github.com>
2024-04-10 13:09:43 -0500

    try fix

--- projects/project3/requires/head.php
+++ projects/project3/requires/head.php
@@ -2,7 +2,7 @@
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
-    <title><? $page_title ?></title>
+    <title><?= $page_title ?></title>
 
     <link href="https://unpkg.com/@primer/css@^20.2.4/dist/primer.css" rel="stylesheet">
 </head>
