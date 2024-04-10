c7c7a6946080fd9bd5fc2ae7fe00895ece6e554d
vandoors <143453791+vandoors@users.noreply.github.com>
2024-04-10 15:35:17 -0500

    update header

--- projects/project3/requires/header.php
+++ projects/project3/requires/header.php
@@ -10,10 +10,10 @@
     <h1 class="h2"><?= $page_title ?></h1>
     <nav>
         <ul>
-            <li class="d-inline-block"><a href=<?= dirname($_SERVER['PHP_SELF']) ?>>Home</a></li>
             <?php if (!isset($_SESSION['username'])): ?>
-                <a href=<?= dirname($_SERVER['PHP_SELF']) . '/profile' ?>>Profile</a>
+                <li class="d-inline-block"><a href=<?= dirname($_SERVER['PHP_SELF']) ?>>Home</a></li>
             <?php else: ?>
+                <a href=<?= dirname($_SERVER['PHP_SELF']) . '/profile' ?>>Profile</a>
                 <a href=<?= dirname($_SERVER['PHP_SELF']) . '/logout.php' ?>>Logout (<?=$_SESSION['username'] ?>)</a>
             <?php endif; ?>
         </ul>
