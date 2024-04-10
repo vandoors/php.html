69efe4303ca4832577b7bfdfeffb16bf9fd3ac9f
vandoors <143453791+vandoors@users.noreply.github.com>
2024-04-10 14:01:18 -0500

    mb3

--- projects/project3/requires/signup.php
+++ projects/project3/requires/signup.php
@@ -9,25 +9,25 @@
     if ($show_sign_up_form):
 ?>
 <form method="POST" action="<?= $_SERVER['PHP_SELF'] ?>">
-    <div class="form-group">
-        <div class="form-group-header">
-            <label for="firstName" class="form-label">Username</label>
+    <div class="mb-3">
+        <div class="form-group">
+            <div class="form-group-header">
+                <label for="firstName" class="form-label">Username</label>
+            </div>
+            <div class="form-group-body">
+                <input type="text" maxlength="24" class="form-control" id="username" name="username" required>
+            <div>
         </div>
-        <div class="form-group-body">
-            <input type="text" maxlength="24" class="form-control" id="username" name="username" required>
-        <div>
-    </div>
 
-    <div class="form-group">
-        <div class="form-group-header">
-            <label for="firstName" class="form-label">Password</label>
+        <div class="form-group">
+            <div class="form-group-header">
+                <label for="firstName" class="form-label">Password</label>
+            </div>
+            <div class="form-group-body">
+                <input type="password" maxlength="100" class="form-control" id="password" name="password" required>
+            <div>
         </div>
-        <div class="form-group-body">
-            <input type="password" maxlength="100" class="form-control" id="password" name="password" required>
-        <div>
     </div>
-
-    <hr>
 
     <div class="form-group">
         <div class="form-group-header">
