95b199ee80c905e693b90abf23e344b2a4a80f64
vandoors <143453791+vandoors@users.noreply.github.com>
2024-04-10 13:43:36 -0500

    styling

--- projects/project3/requires/header.php
+++ projects/project3/requires/header.php
@@ -1,9 +1,9 @@
-<header class="mb-3">
+<header class="mb-6">
     <h1 class="h2"><?= $page_title ?></h1>
     <nav>
         <ul>
-            <li><a href=<?= dirname($_SERVER['PHP_SELF']) ?>>Home</a></li>
-            <li><a href=<?= dirname($_SERVER['PHP_SELF']) . '/profile' ?>>Profile</a></li>
+            <li class="d-inline-block"><a href=<?= dirname($_SERVER['PHP_SELF']) ?>>Home</a></li>
+            <li class="d-inline-block"><a href=<?= dirname($_SERVER['PHP_SELF']) . '/profile' ?>>Profile</a></li>
         </ul>
     </nav>
 
