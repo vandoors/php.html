9918e4fab6450b0010fb6cf3dabc9244fdda0680
vandoors <143453791+vandoors@users.noreply.github.com>
2024-04-10 17:12:17 -0500

    u

--- projects/project3/requires/profile/edit.php
+++ projects/project3/requires/profile/edit.php
@@ -1,10 +1,21 @@
 <form method="POST" action="<?= $_SERVER['PHP_SELF'] ?>">
-    <div class="form-group">
-        <div class="form-group-header">
-            <label for="firstName" class="form-label">Username</label>
+    <div class="mb-6">
+        <div class="form-group">
+            <div class="form-group-header">
+                <label for="firstName" class="form-label">Username</label>
+            </div>
+            <div class="form-group-body">
+                <input type="text" maxlength="24" class="form-control" id="username" name="username" value="<?=$username?>" disabled>
+            </div>
         </div>
-        <div class="form-group-body">
-            <input type="text" maxlength="24" class="form-control" id="username" name="username" value="<?=$username?>" required>
+
+        <div class="form-group">
+            <div class="form-group-header">
+                <label for="firstName" class="form-label">Password</label>
+            </div>
+            <div class="form-group-body">
+                <input type="password" maxlength="100" class="form-control" id="password" name="password" required>
+            </div>
         </div>
     </div>
 
