4983bbc12acfc9ad7d669ddb1048bf1de178bad6
vandoors <143453791+vandoors@users.noreply.github.com>
2024-04-10 20:04:55 -0500

    del

--- projects/project3/requires/logger/deletelog.php
+++ projects/project3/requires/logger/deletelog.php
@@ -1,1 +1,21 @@
 <?php
+if (isset($_POST['delete_log']))
+{
+    if (session_status() == PHP_SESSION_NONE)
+    {
+        session_start();
+    }
+
+    $dbc = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME)
+        or trigger_error(
+                'Error connecting to MySQL server for DB_NAME.', 
+                E_USER_ERROR
+        );
+
+    $query = "DELETE FROM exercise_log WHERE id = ? AND user_id = ?";
+
+    parameterizedQuery($dbc, $query, 'ii', $_POST['log_id'], $_SESSION['id'])
+        or trigger_error(mysqli_error($dbc), E_USER_ERROR);
+
+    mysqli_close($dbc);
+}
