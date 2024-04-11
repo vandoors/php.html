377139738a529ddc5508ef4b05900c103f2ee826
vandoors <143453791+vandoors@users.noreply.github.com>
2024-04-10 20:21:32 -0500

    f

--- projects/project3/requires/home/main.php
+++ projects/project3/requires/home/main.php
@@ -3,13 +3,13 @@
     To get started, please create a profile below. Otherwise, if you already have a profile, please log in.
 </p>
 
-<div class="index-grid">
-    <div class="mt-3 sign-up">
+<div class="d-flex flex-row">
+    <div>
         <h2 class="h3">Create a profile</h2>
         <?php require_once('./requires/signup/main.php'); ?>
     </div>
 
-    <div class="log-in>
+    <div>
         <h2 class="h3">Log in</h2>
         <?php require_once('./requires/login/main.php'); ?>
     </div>
