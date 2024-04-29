5632031b8d43cd6e013949f74433816b01508236
vandoors <143453791+vandoors@users.noreply.github.com>
2024-04-29 17:41:09 -0500

    .

--- projects/project4/index.php
+++ projects/project4/index.php
@@ -7,12 +7,8 @@ $page_title = 'Twitter';
 
 <body>
     <?php if (!isset($_SESSION['id'])) : ?>
-        <?php require_once('./requires/home/main.php'); ?>
+        <?php require_once('./home/main.php'); ?>
     <?php else : ?>
-        <div class="index-grid">
-            <?php require_once('./requires/profile/main.php'); ?>
-            <?php require_once('./requires/logger/main.php'); ?>
-        </div>
     <?php endif; ?>
 </body>
 
