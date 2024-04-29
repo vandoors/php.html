6ebcd23bf5a860fadb1d3784538202a0dd3482ae
vandoors <143453791+vandoors@users.noreply.github.com>
2024-04-29 17:30:50 -0500

    project 4 start

---
+++ projects/project4/globals.css.php
@@ -0,0 +1,9 @@
+<style type="text/tailwindcss">
+   @tailwind base;
+   @tailwind components;
+   @tailwind utilities;
+
+   body {
+      @apply bg-black;
+   }
+</style>
---
+++ projects/project4/partials/head.php
@@ -0,0 +1,9 @@
+<head>
+   <meta charset="utf-8" />
+   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+   <title><?= $page_title ?></title>
+
+   <script src="https://cdn.tailwindcss.com"></script>
+   <?php require_once('./globals.css.php'); ?>
+</head>
