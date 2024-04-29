bcdb81981ad8daeb001c527c8c0aef1d9d570fc1
vandoors <143453791+vandoors@users.noreply.github.com>
2024-04-29 18:52:13 -0500

    Update head.php

--- projects/project4/_partials/head.php
+++ projects/project4/_partials/head.php
@@ -5,5 +5,8 @@
    <title><?= $page_title ?></title>
 
    <script src="https://cdn.tailwindcss.com"></script>
-   <?php require_once('../globals.css.php'); ?>
+   <?php
+   $home_url = $_SERVER['DOCUMENT_ROOT'];
+   require_once($home_url . '/globals.css.php');
+   ?>
 </head>
