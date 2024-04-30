ba946cbbd9dbe6ee1b133ba6e0261a50bea93833
vandoors <143453791+vandoors@users.noreply.github.com>
2024-04-29 23:09:06 -0500

    Update edit.php

--- projects/project4/profile/edit.php
+++ projects/project4/profile/edit.php
@@ -6,6 +6,17 @@
       require_once('/var/www/html/projects/project4/tweet.php');
    }
    ?>
+   <form method="POST" action="<?= $_SERVER['PHP_SELF'] ?>" class="pt-4">
+      <textarea class="block w-full bg-gray-200 rounded-lg px-2 py-1" name="content" maxlength="140" required></textarea>
+
+      <button type="submit" class="block mt-2 px-4 py-2 bg-sky-400 text-white hover:opacity-90 rounded-md" name="tweet">Tweet</button>
+   </form>
+
+   <?php
+   if (isset($_POST['edit_profile']) && isset($_POST['content'])) {
+      require_once('/var/www/html/projects/project4/tweet.php');
+   }
+   ?>
 
    <?php
    require_once('/var/www/html/projects/project4/dbconnection.php');
