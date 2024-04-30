ca90ca4eee62969e84f2750c346537afcd9af4ce
vandoors <143453791+vandoors@users.noreply.github.com>
2024-04-29 20:34:02 -0500

    Update default.php

--- projects/project4/home/default.php
+++ projects/project4/home/default.php
@@ -2,5 +2,5 @@
    <p class="font-semibold text-lg">Sign up to enjoy Twitter</p>
 
    <a href="signup" class="mt-6 block bg-black p-3 text-center text-lg text-white font-bold rounded-full hover:opacity-90">Sign Up</a>
-   <a href="login" class="mt-4 block border-black border-2 p-3 text-center text-lg font-bold rounded-full decoration-4 hover:underline">Log In</a>
+   <a href="login" class="mt-4 block border-black border-2 p-3 text-center text-lg font-bold rounded-full decoration-4 underline-offset-4 hover:underline">Log In</a>
 </div>
