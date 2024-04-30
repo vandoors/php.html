263c58a4404c56ce89086f1a1de417b89b501a91
vandoors <143453791+vandoors@users.noreply.github.com>
2024-04-29 20:28:55 -0500

    Update default.php

--- projects/project4/home/default.php
+++ projects/project4/home/default.php
@@ -2,5 +2,5 @@
    <p class="font-semibold text-lg">Sign up to enjoy Twitter</p>
 
    <a href="signup" class="block bg-black p-3 text-center text-lg text-white font-bold rounded-full">Sign Up</a>
-   <a href="login" class="block border-black border-2 p-3 text-center text-lg text-white font-bold rounded-full">Log In</a>
+   <a href="login" class="block border-black border-2 p-3 text-center text-lg font-bold rounded-full">Log In</a>
 </div>
