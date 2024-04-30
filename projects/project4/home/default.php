bee2d023f368230c3c40d2fe8a1d87629c45fcf9
vandoors <143453791+vandoors@users.noreply.github.com>
2024-04-29 20:25:06 -0500

    .

---
+++ projects/project4/home/default.php
@@ -0,0 +1,6 @@
+<div>
+   <p class="font-semibold text-lg">See what's happening right now</p>
+
+   <a href="signup" class="block bg-black p-3 text-center ">Sign Up</a>
+   <a href="login">Log In</a>
+</div>
