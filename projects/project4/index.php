22b3271eac5ffa77f5670c5d2ccf0b6f3f6bfbaf
vandoors <143453791+vandoors@users.noreply.github.com>
2024-04-29 19:04:48 -0500

    Update index.php

--- projects/project4/index.php
+++ projects/project4/index.php
@@ -7,7 +7,7 @@ $page_title = 'Twitter';
 
 <body>
     <div class="flex w-full justify-center">
-        <div class="flex w-full max-w-[70rem] flex-row gap-2 md:h-full">
+        <div class="flex w-full max-w-4xl flex-row gap-2 md:h-full">
             <aside class="sticky top-0 w-1/3 self-start overflow-y-scroll">
                 <?php if (!isset($_SESSION['id'])) : ?>
                     <?php require_once('./home/page.php'); ?>
