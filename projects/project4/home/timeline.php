03f16e8901bbcbfc06cb058fcf62595167a43da7
vandoors <143453791+vandoors@users.noreply.github.com>
2024-04-29 23:42:46 -0500

    Update timeline.php

--- projects/project4/home/timeline.php
+++ projects/project4/home/timeline.php
@@ -3,6 +3,31 @@
    require_once('/var/www/html/projects/project4/dbconnection.php');
    require_once('/var/www/html/projects/project4/queryutils.php');
 
+   if (isset($_POST['delete_tweet'])) {
+      $tweet_id = $_POST['tweet_id'];
+
+      $dbc = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME)
+         or trigger_error(
+            'Error connecting to MySQL server for DB_NAME.',
+            E_USER_ERROR
+         );
+
+      $query_for_user_of_tweet_to_delete = "SELECT `user_id` FROM twitter_tweet WHERE id = ?";
+      $result_for_user_of_tweet_to_delete = parameterizedQuery($dbc, $query_for_user_of_tweet_to_delete, 'i', $tweet_id)
+         or trigger_error(mysqli_error($dbc), E_USER_ERROR);
+
+      $row_for_user_of_tweet_to_delete = mysqli_fetch_array($result_for_user_of_tweet_to_delete);
+      $user_id = $row_for_user_of_tweet_to_delete['user_id'];
+
+      if ($user_id != $_SESSION['id']) {
+         echo "<p class='text-red-500'>You do not have permission to delete this tweet.</p>";
+      } else {
+         $query = "DELETE FROM twitter_tweet WHERE id = ?";
+         parameterizedQuery($dbc, $query, 'i', $tweet_id)
+            or trigger_error(mysqli_error($dbc), E_USER_ERROR);
+      }
+   }
+
    $dbc = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME)
       or trigger_error(
          'Error connecting to MySQL server for DB_NAME.',
@@ -37,6 +62,14 @@
          echo "<span class='text-gray-600'>$date</span>";
          echo "</header>";
          echo "<p class='tweet-content'>$content</p>";
+
+         if ($user_id == $_SESSION['id']) {
+            echo "<form method='POST' action='" . $_SERVER['PHP_SELF'] . "' class='pt-2'>";
+            echo "<input type='hidden' name='tweet_id' value='$tweet_id'>";
+            echo "<button type='submit' class='block ml-auto text-right text-gray-400 hover:text-red-500' name='delete_tweet'>Delete</button>";
+            echo "</form>";
+         }
+
          echo "</article>";
       }
    } else {
