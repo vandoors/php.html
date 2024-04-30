1cd732651ffd8799fac921f961141e2f11d39c16
vandoors <143453791+vandoors@users.noreply.github.com>
2024-04-29 20:48:29 -0500

    Update page.php

--- projects/project4/signup/page.php
+++ projects/project4/signup/page.php
@@ -8,29 +8,14 @@ if (isset($_POST['create_profile'])) {
 if ($show_sign_up_form) :
 ?>
     <form method="POST" action="<?= $_SERVER['PHP_SELF'] ?>">
-        <div class="mb-6">
-            <div class="form-group">
-                <div class="form-group-header">
-                    <label for="firstName" class="form-label">Username</label>
-                </div>
-                <div class="form-group-body">
-                    <input type="text" maxlength="24" class="form-control" id="username" name="username" required>
-                </div>
-            </div>
+        <div>
+            <label for="firstName" class="text-lg font-medium">Username</label>
+            <input type="text" maxlength="24" class="form-control" id="username" name="username" required>
 
-            <div class="form-group">
-                <div class="form-group-header">
-                    <label for="firstName" class="form-label">Password</label>
-                </div>
-                <div class="form-group-body">
-                    <input type="password" maxlength="100" class="form-control" id="password" name="password" required>
-                </div>
-            </div>
-        </div>
+            <label for="firstName" class="form-label">Password</label>
+            <input type="password" maxlength="100" class="form-control" id="password" name="password" required>
 
-        <div class="form-group">
             <button type="submit" class="btn btn-primary mt-3" name="create_profile">Create profile</button>
-        </div>
     </form>
 <?php
 endif;
