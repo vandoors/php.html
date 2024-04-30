5b348a448551560b8007d9f61a7d12620b3b1077
vandoors <143453791+vandoors@users.noreply.github.com>
2024-04-29 20:28:40 -0500

    Update default.php

--- projects/project4/home/default.php
+++ projects/project4/home/default.php
@@ -1,6 +1,6 @@
-<div>
-   <p class="font-semibold text-lg">See what's happening right now</p>
+<div class="pt-2">
+   <p class="font-semibold text-lg">Sign up to enjoy Twitter</p>
 
-   <a href="signup" class="block bg-black p-3 text-center text-white font-bold rounded-full">Sign Up</a>
-   <a href="login">Log In</a>
+   <a href="signup" class="block bg-black p-3 text-center text-lg text-white font-bold rounded-full">Sign Up</a>
+   <a href="login" class="block border-black border-2 p-3 text-center text-lg text-white font-bold rounded-full">Log In</a>
 </div>
