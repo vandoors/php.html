ff4c80dc66a9818a4851ffbc21a863549d702c14
vandoors <143453791+vandoors@users.noreply.github.com>
2024-04-29 20:55:00 -0500

    Update page.php

--- projects/project4/signup/page.php
+++ projects/project4/signup/page.php
@@ -7,7 +7,7 @@ if (isset($_POST['create_profile'])) {
 
 if ($show_sign_up_form) :
 ?>
-    <form method="POST" action="<?= $_SERVER['PHP_SELF'] ?>">
+    <form method="POST" action="<?= $_SERVER['PHP_SELF'] ?>" class="px-4">
         <div>
             <label for="firstName" class="text-lg font-medium">Username</label>
             <input type="text" maxlength="24" class="block mb-4 bg-gray-200 rounded-lg px-2 py-1" id="username" name="username" required>
@@ -15,7 +15,7 @@ if ($show_sign_up_form) :
             <label for="firstName" class="text-lg font-medium">Password</label>
             <input type="password" maxlength="100" class="block mb-4 bg-gray-200 rounded-lg px-2 py-1" id="password" name="password" required>
 
-            <button type="submit" class="block" name="create_profile">Create profile</button>
+            <button type="submit" class="block px-2 py-1 bg-sky-400 text-white hover:opacity-90" name="create_profile">Create profile</button>
     </form>
 <?php
 endif;
