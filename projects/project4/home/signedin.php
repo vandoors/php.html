26bbc9a049a682952e2e8961a62ebeba06921b31
vandoors <143453791+vandoors@users.noreply.github.com>
2024-04-29 22:07:14 -0500

    .

--- projects/project4/home/signedin.php
+++ projects/project4/home/signedin.php
@@ -1,5 +1,9 @@
 <div class="pt-2">
    <p class="font-semibold text-lg">What's happening?!</p>
 
-   <p>you are signed in</p>
+   <form method="POST" action="<?= $_SERVER['PHP_SELF'] ?>">
+      <textarea class="block w-full bg-gray-200 rounded-lg px-2 py-1" name="tweet" maxlength="140" required></textarea>
+
+      <button type="submit" class="block px-2 py-1 bg-sky-400 text-white hover:opacity-90 rounded-md" name="tweet">Tweet</button>
+   </form>
 </div>
