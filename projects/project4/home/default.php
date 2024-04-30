097321dac1e20df284a40eb98f504d9be21145c2
vandoors <143453791+vandoors@users.noreply.github.com>
2024-04-29 20:39:04 -0500

    .

--- projects/project4/home/default.php
+++ projects/project4/home/default.php
@@ -1,8 +1,6 @@
 <div class="pt-2">
    <p class="font-semibold text-lg">Sign up to enjoy Twitter</p>
 
-   <div class="pr-4">
-      <a href="signup" class="mt-6 block bg-black p-3 text-center text-lg text-white font-bold rounded-full hover:opacity-90">Sign Up</a>
-      <a href="login" class="mt-4 block border-black border-2 p-3 text-center text-lg font-bold rounded-full decoration-4 underline-offset-4 hover:underline">Log In</a>
-   </div>
+   <a href="signup" class="mt-6 block bg-black p-3 text-center text-lg text-white font-bold rounded-full hover:opacity-90">Sign Up</a>
+   <a href="login" class="mt-4 block border-black border-2 p-3 text-center text-lg font-bold rounded-full decoration-4 underline-offset-4 hover:underline">Log In</a>
 </div>
